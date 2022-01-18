5f042cdcb3eb378e7fe143722521b0722e9d12b6
SeongHyeon.Seo <tintachina84@gmail.com>
2022-01-18 22:40:14 +0900

    First commit.

---
+++ src/main/java/com/tintachina/experimental/ComplimentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tintachina.experimental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplimentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplimentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tintachina/experimental/ComplimentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tintachina.experimental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplimentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
