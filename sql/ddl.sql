e57094b13ac17c380659e6dee5d9074bf32076ce
SeongHyeon.Seo <tintachina84@gmail.com>
2022-01-20 22:36:51 +0900

    Modified ddl and init query.

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -9,14 +9,22 @@ GRANT ALL PRIVILEGES ON compl.* TO 'compluser'@'%';
 use compl;
 
 create table compl.user (
-  user_seq varchar(50) not null primary key,
-  user_id varchar(50),
+  user_id varchar(50) not null primary key,,
   user_name varchar(50),
+  user_password varchar(50),
   state varchar(20),
   point bigint,
+  sent_point bigint,
+  received_point bigint,
   created_at datetime,
   updated_at datetime,
   deleted_at datetime
+) character set utf8;
+
+create table compl.user_authorities (
+  user_id varchar(50) not null,
+  authority varchar(50) not null,
+  primary key (user_id, authority)
 ) character set utf8;
 
 create table compl.compliment (
@@ -27,8 +35,23 @@ create table compl.compliment (
   receiver_name varchar(50),
   compliment_point bigint,
   reward_point bigint,
-  compliment_message varchar(200),
+  compliment_message varchar(2000),
   state varchar(20),
+  created_at datetime,
+  updated_at datetime,
+  deleted_at datetime
+) character set utf8;
+
+create table compl.compliment_like (
+  like_seq varchar(50) not null primary key,
+  compliment_seq varchar(50),
+  user_id varchar(50)
+) character set utf8;
+
+create table compl.log (
+  log_seq varchar(50) not null primary key,
+  log varchar(2000),
+  user_id varchar(50),
   created_at datetime,
   updated_at datetime,
   deleted_at datetime
--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,14 @@
 use compl;
 
 truncate table user;
+truncate table user_authorities;
 truncate table compliment;
+truncate table compliment_like;
+truncate table log;
+
+insert into user (user_id, user_name, user_password, state, point, created_at) values ('user1', '사용자1', '1234', 'Y', 100000, NOW());
+insert into user (user_id, user_name, user_password, state, point, created_at) values ('user2', '사용자2', '1234', 'Y', 100000, NOW());
+insert into user (user_id, user_name, user_password, state, point, created_at) values ('admin', '운영자', '1234', 'Y', 100000, NOW());
+insert into user_authorities values ('user1', 'ROLE_USER');
+insert into user_authorities values ('user2', 'ROLE_USER');
+insert into userr_authorities values ('admin', 'ROLE_ADMIN');
