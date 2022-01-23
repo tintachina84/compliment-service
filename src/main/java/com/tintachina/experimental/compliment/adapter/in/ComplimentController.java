c71e194011356236337b6072b5a2ac79d900f605
SeongHyeon.Seo <tintachina84@gmail.com>
2022-01-23 20:54:41 +0900

    Added HeathCheck test method.

--- src/main/java/com/tintachina/experimental/compliment/adapter/in/ComplimentController.java
+++ src/main/java/com/tintachina/experimental/compliment/adapter/in/ComplimentController.java
@@ -1,5 +1,16 @@
 package com.tintachina.experimental.compliment.adapter.in;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ComplimentController {
     
+    @GetMapping(value = "healthcheck")
+    public ResponseEntity<String> healthcheck() {
+        return new ResponseEntity<String>("OK", HttpStatus.OK);
+    }
 }
